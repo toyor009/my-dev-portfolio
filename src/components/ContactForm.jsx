a2cf2a7dae2ba5385491d1a8fdfa2069384a92ad
Toyosi Olayiwola <toyorboy012@gmail.com>
2024-09-11 16:21:33 +0100

    update contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,9 +1,35 @@
-import socials from '../content/socials';
+import { useState, useMemo } from 'react';
+
 import { MdEmail } from 'react-icons/md';
 import { FaPhoneAlt } from 'react-icons/fa';
 import { IoLocation } from 'react-icons/io5';
 
+import socials from '../content/socials';
+
 const ContactForm = () => {
+  const emptyForm = {
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  };
+
+  const [form, setForm] = useState({ ...emptyForm });
+
+  const formIsFilled = useMemo(() => {
+    return Object.values(form).every((field) => field.length > 0);
+  }, [form]);
+
+  const updateContactForm = (field, value) => {
+    setForm({ ...form, [field]: value });
+  };
+
+  const submitForm = () => {
+    console.log('form: ', form);
+    console.log('formIsFilled: ', formIsFilled);
+    if (!formIsFilled) return;
+  };
+
   return (
     <div className='contact-section'>
       <div className='contact-info'>
@@ -40,22 +66,41 @@ const ContactForm = () => {
           <br />
         </div>
       </div>
+
       <div className='contact-form'>
         <form name='contact'>
           <input
             type='text'
             name='senderName'
             placeholder='Your Name'
+            value={form.name}
             required
+            onInput={(e) => updateContactForm('name', e.target.value)}
           />
-          <input type='text' name='senderEmail' placeholder='Email' required />
-          <input type='text' name='subject' placeholder='Subject' required />
+          <input
+            type='email'
+            name='senderEmail'
+            placeholder='Email'
+            value={form.email}
+            required
+            onInput={(e) => updateContactForm('email', e.target.value)}
+          />
+          <input
+            type='text'
+            name='subject'
+            placeholder='Subject'
+            value={form.subject}
+            required
+            onInput={(e) => updateContactForm('subject', e.target.value)}
+          />
           <textarea
             placeholder='Your Message'
             name='message'
+            value={form.message}
             required
+            onInput={(e) => updateContactForm('message', e.target.value)}
           ></textarea>
-          <input type='submit' value='Send' />
+          <input type='button' value='Send' onClick={submitForm} />
         </form>
       </div>
     </div>
